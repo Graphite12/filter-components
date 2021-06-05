fd4a8101f70e2a446cdcb856135a05c46653ecbf
hsh411 <pixeldot12@gmail.com>
2021-06-05 21:40:29 +0900

    modify css styling

--- filter-app/src/components/Card.js
+++ filter-app/src/components/Card.js
@@ -7,17 +7,19 @@ export default function Card({ card }) {
   console.log(spec.architecture);
   return (
     <>
-      <header>
+      <header className="card_header">
         <h1>{model}</h1>
       </header>
       <figure className="image_box">
         <img src={image} />
       </figure>
-      <span>제조사: {company}</span>
-      <span>종류: {kind}</span>
-      <span>모델명: {model}</span>
-      <span>아키텍쳐: {spec.architecture}</span>
-      <span>메모리: {spec.vram}</span>
+      <div className="card_info">
+        <span>제조사: {company}</span>
+        <span>종류: {kind}</span>
+        <span>모델명: {model}</span>
+        <span>아키텍쳐: {spec.architecture}</span>
+        <span>메모리: {spec.vram}</span>
+      </div>
     </>
   );
 }
